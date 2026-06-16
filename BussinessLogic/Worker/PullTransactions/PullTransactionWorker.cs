7dbd256c2be12bd9c14d24b853ea026e4726a8ce
Nicholas Waweru <nicholaswawerunjeri@gmail.com>
2026-06-16 20:48:32 +0300

    all errors removed

--- BussinessLogic/Worker/PullTransactions/PullTransactionWorker.cs
+++ BussinessLogic/Worker/PullTransactions/PullTransactionWorker.cs
@@ -1,5 +1,5 @@
 ﻿
-using BussinessLogic.Services.Daraja;
+using FuelFlow.Services.Daraja;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
