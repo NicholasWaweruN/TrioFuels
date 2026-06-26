91cc9f692959a4255771e253bf44ddd59361ca11
Nicholas Waweru <nicholaswawerunjeri@gmail.com>
2026-06-26 12:35:26 +0300

    0712345678

--- DataAccessLayer/DTOs/Customer/CustomerDTO.cs
+++ DataAccessLayer/DTOs/Customer/CustomerDTO.cs
@@ -8,12 +8,11 @@ namespace DataAccessLayer.DTOs.Customer
 
 	public class CustomerDTO
 	{
-		[Required]
 		public string CustomerName { get; set; } = string.Empty;
 
 		private string _customerPhone = string.Empty;
 
-		[Required]
+	
 		[RegularExpression(@"^((\+2547\d{8})|(07\d{8})|(\+2541\d{8})|(01\d{8}))$",ErrorMessage = "Enter a valid Kenyan phone number (e.g., 0712345678 or +254712345678).")]
 		public string CustomerPhone { get => _customerPhone; set => _customerPhone = value?.Trim() ?? string.Empty; }
 
@@ -27,9 +26,6 @@ namespace DataAccessLayer.DTOs.Customer
 
 		[RegularExpression(@"^[A-Z]{1}[0-9]{9}[A-Z]{1}$",ErrorMessage = "KRA PIN must be 11 characters long (e.g., A123456789B).")]
 		public string Krapin { get => _krapin;set => _krapin = value?.Trim().ToUpper() ?? string.Empty;}
-
-	
-		[StringLength(8, MinimumLength = 7,ErrorMessage = "Identification number must be 7 or 8 characters long.")]
 		public string IdentificationNumber { get; set; } = string.Empty;
 		public bool IsCreditCustomer { get; set; } = false;
 		public decimal CreditLimit { get; set; } = 0m;
