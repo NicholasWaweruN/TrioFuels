4cbfa6443393e83f2e47c6b073683e093ff79149
Nicholas Waweru <nicholaswawerunjeri@gmail.com>
2026-07-09 23:21:16 +0300

    Nozzle transfer allowed only when shift is in Variance solved this message

--- BussinessLogic/Sales/ReverseSales/ReverseSales.cs
+++ BussinessLogic/Sales/ReverseSales/ReverseSales.cs
@@ -175,7 +175,7 @@ namespace BussinessLogic.Sales.ReverseSales
 					if (shift == null)
 						return ServiceResponse<object>.Information("Shift not found", null);
 
-					if (shift.ShiftStatus != ShiftStatus.Variance)
+					if (shift.ShiftStatus != ShiftStatus.Variance || shift.ShiftStatus != ShiftStatus.Pending )
 						return ServiceResponse<object>.Information("Nozzle transfer allowed only when shift is in Variance", null);
 
 					if (sale.IsReversed)
