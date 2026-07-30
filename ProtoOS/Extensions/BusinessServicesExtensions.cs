a870f0a7ca9c910e37775d3e6fe5dfe58f347ae0
Nicholas Waweru <nicholaswawerunjeri@gmail.com>
2026-07-30 18:57:15 +0300

    RESOLVING DEPEDENCY INJECTION ERROR

--- ProtoOS/Extensions/BusinessServicesExtensions.cs
+++ ProtoOS/Extensions/BusinessServicesExtensions.cs
@@ -196,6 +196,8 @@ public static class BusinessServicesExtensions
 		services.AddScoped<ICarWashShiftService, CarWashShiftService>();
 		services.AddScoped<ICarWashSalesService, CarWashSalesService>();
 		services.AddScoped<ICreditManagement, CreditManagement>();
+		services.AddScoped<IVehicleSales, VehicleSales>();
+
 
 		services.AddScoped<ICarWashDashboardService, CarWashDashboardService>();
 		services.AddScoped<ICarwashCustomerService, CarwashCustomerService>();
